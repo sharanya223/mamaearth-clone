ce310cbb1f46c5ec065c0b1f06ac15d39ace2a32
sharanya <sharanyar716@gmail.com>
2026-04-17 20:06:39 +0530

    Fixed css file import

--- mamaearth-react/src/pages/productDetailes.jsx
+++ mamaearth-react/src/pages/productDetailes.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import "./ProductDetailes.css";
+import "./productDetailes.css";
 import { useNavigate } from "react-router-dom";
 
 function ProductDetails() {
