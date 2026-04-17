f41ec188886c2127eb9e84a80a613000b9018959
sharanya <sharanyar716@gmail.com>
2026-04-17 20:15:24 +0530

    Fixed cart css import

--- mamaearth-react/src/pages/cart.jsx
+++ mamaearth-react/src/pages/cart.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./Cart.css";
+import "./cart.css";
 
 function ProductCart() {
   const [cartItems, setCartItems] = useState([]);
